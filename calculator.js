c2475e94f2b773a8d34ff51f8cb4548865bbaf51
Akanksha Yadav <86938578+Akku22jan@users.noreply.github.com>
2024-07-03 16:02:50 +0530

    Day 4
    I. Enabled backspace key. 
    II. Started working on theme 1 for mobile. Finished the layout and color scheme.

--- calculator.js
+++ calculator.js
@@ -3,21 +3,8 @@ const operators = Array.from(document.getElementsByClassName("operator"));
 const display = document.getElementById("display");
 const reset = document.getElementById("reset");
 const del = document.getElementById('del');
-//let expression;
-let numbers = [];
-//let result;
-let seperator;
 
-/****To display the number in the display section when the buttons are clicked ******/
-
-digits.forEach(accessButton);
-
-function accessButton(digit) {
-    digit.addEventListener("click", function displayNumber() {
-        display.innerHTML += digit.innerHTML;
-    });
-}
-
+/********    Typing in the calculator through keys */
 window.addEventListener("keydown", () => {
     let key = event.key;
 
@@ -29,8 +16,7 @@ window.addEventListener("keydown", () => {
     }
 
     if(key === '='){
-        const expression = display.innerHTML.slice(1, length - 1); //getting rid of equal to sign
-            //result = math.evaluate(expression);
+            const expression = display.innerHTML.slice(1, length - 1); //getting rid of equal to sign
             const func = new Function("return " + expression);
             const result = func();
             display.innerHTML += `<p>${result}</p>`;
@@ -44,8 +30,25 @@ window.addEventListener("keydown", () => {
             const result = func();
             display.innerHTML += `<p>${result}</p>`;
      }
+     /*** delete the last character */
+     if(key === 'Backspace'){
+        let length = display.innerHTML.length;
+        display.innerHTML = display.innerHTML.slice(0, length - 1);
+     }
 
 });
+
+
+/****To display the number in the display section when the buttons are clicked ******/
+
+digits.forEach(accessButton);
+
+function accessButton(digit) {
+    digit.addEventListener("click", function displayNumber() {
+        display.innerHTML += digit.innerHTML;
+    });
+}
+
 /****To display the operator in the display section when the buttons are clicked ******/
 
 operators.forEach(accessOperator);
@@ -58,68 +61,12 @@ function accessOperator(operator) {
             display.innerHTML += operator.innerHTML;
         }
 
-        /*     
-             /**** perform the operator *****
-             if(operator.id ==="add"){ 
-                 seperator = "+";
-             }
-     
-             if(operator.id ==="subtract"){ 
-                 seperator = "-";
-             }
-     
-             
-     
-             if(operator.id ==="divide"){ 
-                 seperator = "/";
-             }
-     */
         /* Evaluation of the expression */
 
         if (operator.id === "result") {
             const expression = display.innerHTML.slice(1, length - 1); //getting rid of equal to sign
-            //result = math.evaluate(expression);
             const func = new Function("return " + expression);
             const result = func();
-            /*
-            numbers = expression.split(seperator); // removing +, -, x, /, just getting array of all the numbers
-
-            if(seperator === "+"){
-                result = numbers.reduce(addition,0);
-            } else if(seperator === "-"){
-                result = numbers.reduce(subtraction);
-            } else if(seperator === "x"){
-                result = numbers.reduce(multiplication,1);
-            } else if(seperator === "/"){
-                result = numbers.reduce(division);
-            }
-
-
-
-            /*addition *
-            function addition(result,num){
-                num = parseFloat(num);
-                return result + num;
-            }
-
-            /*subtraction *
-            function subtraction(result,num){
-                num = parseFloat(num);
-                return result - num;
-            }
-
-            /*multiplication *
-            function multiplication(result,num){
-                num = parseFloat(num);
-                return result * num;
-            }
-
-            /*division *
-            function division(result,num){
-                num = parseFloat(num);
-                return result / num;
-            }
-            */
             display.innerHTML += `<p>${result}</p>`;
         }
     });
